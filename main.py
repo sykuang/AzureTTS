75b9d0128908efdaffa5df880e0c709195a0f9de
sykuang <sykuang.tw@gmail.com>
2024-06-18 22:15:14 +0800

    feat: Add support for changing speech rate in TTS function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from os import getenv
+from glob import glob
 import azure.cognitiveservices.speech as speechsdk
 import argparse
 
@@ -8,27 +9,31 @@ SPEECH_KEY = getenv("SPEECH_KEY")
 SPEECH_REGION = getenv("SPEECH_REGION")
 
 
-def generate_ssml(text: str, voice_name: str, style: str = "chat", stydegree: int = 1):
+def generate_ssml(text: str, voice_name: str, style: str = "chat", stydegree: int = 1,rate="default"):
     ssml = (
         '<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="https://www.w3.org/2001/mstts" xml:lang="en-US">'
         f'<voice name="{voice_name}">'
+        f'<prosody pitch="default" contour="default" range="default" rate="{rate}" volume="default">'
         f'<mstts:express-as style="{style}" styledegree="{stydegree}">'
         f"{ text }"
         "</mstts:express-as>"
+        '</prosody>'
         "</voice>"
         "</speak>"
     )
+    print(ssml)
     return ssml
 
 
 def tts(
-    text: str, voice_name: str, filename: str, style: str = "chat", stydegree: int = 1
+    text: str, voice_name: str, filename: str, style: str = "chat", stydegree: int = 1,rate="default"
 ):
     speech_config = speechsdk.SpeechConfig(
         subscription=SPEECH_KEY, region=SPEECH_REGION
     )
     if not filename:
         audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+        filename="speaker"
     else:
         audio_config = speechsdk.audio.AudioOutputConfig(filename=filename)
     # The language of the voice that speaks.
@@ -38,14 +43,14 @@ def tts(
         speech_config=speech_config, audio_config=audio_config
     )
 
-    ssml = generate_ssml(text, voice_name, style, stydegree)
+    ssml = generate_ssml(text, voice_name, style, stydegree,rate)
     speech_synthesis_result = speech_synthesizer.speak_ssml_async(ssml).get()
 
     if (
         speech_synthesis_result.reason
         == speechsdk.ResultReason.SynthesizingAudioCompleted
     ):
-        print(f"Speech synthesized for text [{text}]")
+        print(f"Speech synthesized for text [{text}] to [{filename}]")
     elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
         cancellation_details = speech_synthesis_result.cancellation_details
         print(f"Speech synthesis canceled: {cancellation_details.reason}")
@@ -82,6 +87,12 @@ def main():
         default=False,
         help="Play the output or store in filename",
     )
+    parser.add_argument(
+        "--rate",
+        type=str,
+        default="default",
+        help="Play the output or store in filename",
+    )
     parser.add_argument("--script", type=str, help="Script to read")
     args = parser.parse_args()
     if args.script:
@@ -97,11 +108,14 @@ def main():
                     text = ls[1]
                 else:
                     text = line
+
+                while len(glob(f"tts{cnt}.wav"))>0:
+                    cnt+=1
                 tts(text, args.voice, f"tts{cnt}.wav", style, styledegree)
                 cnt += 1
     else:
         filename = "" if args.play else args.filename
-        tts(args.text, args.voice, filename, args.style, args.styledegree)
+        tts(args.text, args.voice, filename, args.style, args.styledegree,args.rate)
 
 
 if __name__ == "__main__":
